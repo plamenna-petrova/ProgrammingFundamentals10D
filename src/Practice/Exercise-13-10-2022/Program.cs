1f9a5b660e8095fcb14332f2fdf64f78ca5447fa
plamenna-petrova <59893040+plamenna-petrova@users.noreply.github.com>
2022-10-16 19:54:20 +0300

    Added the initial folders distribution

---
+++ src/Practice/Exercise-13-10-2022/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exercise_13_10_2022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
