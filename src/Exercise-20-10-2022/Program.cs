c6d0d30369ad76708357d0dcbaf4e41fce9f4a48
plamenna-petrova <59893040+plamenna-petrova@users.noreply.github.com>
2022-10-20 09:30:46 +0300

    Added the Exercise for the 20th October

---
+++ src/Exercise-20-10-2022/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace Exercise_20_10_2022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbersArray = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            int maximumCountOfIdenticalElements = 0;
+
+            int firstElementWithMaximumIdenticalOccurrences = 0;
+
+            for (int i = 0; i < numbersArray.Length; i++)
+            {
+                int currentCountOfIdenticalElements = 0;
+
+                for (int j = 0; j < numbersArray.Length; j++)
+                {
+                    if (numbersArray[i] == numbersArray[j])
+                    {
+                        currentCountOfIdenticalElements++;
+                    }
+                }
+
+                if (currentCountOfIdenticalElements > maximumCountOfIdenticalElements)
+                {
+                    maximumCountOfIdenticalElements = currentCountOfIdenticalElements;
+                    firstElementWithMaximumIdenticalOccurrences = numbersArray[i];
+                }
+            }
+
+            Console.WriteLine(firstElementWithMaximumIdenticalOccurrences);
+        }
+    }
+}
