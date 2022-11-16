d6997dea10c6ebb09058838cd38e0c4a0c900cc1
plamenna-petrova <59893040+plamenna-petrova@users.noreply.github.com>
2022-11-16 21:32:54 +0200

    Added the exercise for the 3rd and the 4th November

---
+++ src/Practice/Exercise-3-4-11-2022/ArrayMethodsManipulation/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace ArrayMethodsManipulation
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arrayToManipulate = Console.ReadLine().Split(' ').ToArray();
+
+            int countOfCommands = int.Parse(Console.ReadLine());
+
+            while (countOfCommands > 0)
+            {
+                string[] commands = Console.ReadLine().Split(' ').ToArray();
+
+                switch (commands[0])
+                {
+                    case "Distinct":
+                        arrayToManipulate = arrayToManipulate.Distinct().ToArray();
+                        break;
+                    case "Reverse":
+                        // arrayToManipulate = arrayToManipulate.Reverse();
+                        Array.Reverse(arrayToManipulate);
+                        break;
+                    case "Replace":
+                        int replacementIndex = int.Parse(commands[1]);
+                        string elementToReplace = commands[2];
+                        arrayToManipulate[replacementIndex] = elementToReplace;
+                        break;
+                }
+
+                countOfCommands--;
+            }
+
+            Console.WriteLine(string.Join(", ", arrayToManipulate));
+        }
+    }
+}
