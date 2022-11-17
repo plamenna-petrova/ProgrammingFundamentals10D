00fac103e797315b72a230abfb7ed63f572eb9c0
plamenna-petrova <59893040+plamenna-petrova@users.noreply.github.com>
2022-11-17 19:31:51 +0200

    Added the task for the 17th November 2022

---
+++ src/Practice/Exercise-17-11-2022/Warehouse/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace Warehouse
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] productNames = Console.ReadLine().Split().ToArray();
+
+            long[] productQuantities = Console.ReadLine().Split().Select(long.Parse).ToArray();
+
+            decimal[] productPrices = Console.ReadLine().Split().Select(decimal.Parse).ToArray();
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+
+                if (command == "done")
+                {
+                    break;
+                }
+
+                int productNameIndex = Array.IndexOf(productNames, command);
+
+                long productQuantity = productQuantities[productNameIndex];
+
+                decimal productPrice = productPrices[productNameIndex];
+
+                string productName = command;
+
+                Console.WriteLine($"{productName} costs: {productPrice}; Avaialable quantity: {productQuantity}");
+            }
+        }
+    }
+}
