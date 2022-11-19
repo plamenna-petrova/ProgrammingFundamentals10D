ed245cea9cbe4b241bbe9144030d062de3b05859
plamenna-petrova <59893040+plamenna-petrova@users.noreply.github.com>
2022-11-19 12:04:26 +0200

    Added the task for the 17th November

---
+++ src/Practice/Exercise-17-11-2022/WarehouseUpdatedVersion/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+
+namespace WarehouseUpdatedVersion
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] productNames = Console.ReadLine().Split().ToArray();
+
+            long[] productQuantities = Console.ReadLine().Split().Select(long.Parse).ToArray();
+
+            decimal[] productPrices = Console.ReadLine().Split().Select(decimal.Parse).ToArray();
+
+            while (true)
+            {
+                string[] commands = Console.ReadLine().Split().ToArray();
+
+                if (commands[0] == "done")
+                {
+                    break;
+                }
+
+                int productIndex = Array.IndexOf(productNames, commands[0]);
+
+                string productNameCommand = commands[0];
+                long productQuantityCommand = long.Parse(commands[1]);
+
+                long productQuantity = 0;
+
+                if (productIndex <= productQuantities.Length - 1)
+                {
+                    productQuantity = productQuantities[productIndex];
+                }
+
+                decimal productPrice = productPrices[productIndex];
+
+                if (productQuantityCommand <= productQuantity)
+                {
+                    Console.WriteLine($"{productNameCommand} x {productQuantityCommand} " +
+                        $"costs {Math.Round(productQuantityCommand * productPrice, 2):F2}");
+                    productQuantities[productIndex] -= productQuantityCommand;
+                }
+                else
+                {
+                    Console.WriteLine($"We do not have enough {productNameCommand}");
+                }
+            }
+        }
+    }
+}
