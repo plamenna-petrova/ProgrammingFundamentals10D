b70331915a2d0ee35db3cead0e309da715f756a3
plamenna-petrova <59893040+plamenna-petrova@users.noreply.github.com>
2022-11-24 14:35:02 +0200

    Added more detailed description for the first homework's tasks

--- src/Practice/Exercise-27-10-2022/FirstHomeworkSolution/Program.cs
+++ src/Practice/Exercise-27-10-2022/FirstHomeworkSolution/Program.cs
@@ -9,6 +9,8 @@ namespace FirstHomeworkSolution
         {
             int[] array = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
 
+            // Task 1 - Find the sum of the negative array elements
+
             int negativeNumbersSum = 0;
 
             foreach (int element in array)
@@ -23,6 +25,8 @@ namespace FirstHomeworkSolution
 
             Console.WriteLine(new string('-', 40));
 
+            // Task 2 - Find the count of the even array elements
+
             int evenElementsCount = 0;
 
             foreach (int element in array)
@@ -36,6 +40,8 @@ namespace FirstHomeworkSolution
             Console.WriteLine(evenElementsCount);
 
             Console.WriteLine(new string('-', 40));
+
+            // Task 3 - Find the average of the positive array elements
 
             double positiveElementsSum = 0;
             double positiveElementsCount = 0;
@@ -55,6 +61,8 @@ namespace FirstHomeworkSolution
 
             Console.WriteLine(new string('-', 40));
 
+            // Task 4 - Find the multiplication of array elements, which are divisible by 7
+
             int elementsDivisibleBySevenMultiplication = 1;
 
             foreach (int element in array)
@@ -64,6 +72,12 @@ namespace FirstHomeworkSolution
                     elementsDivisibleBySevenMultiplication *= element;
                 }
             }
+
+            Console.WriteLine(elementsDivisibleBySevenMultiplication);
+
+            Console.WriteLine(new string('-', 40));
+
+            // Task 5 - Find the minimum array element along with its index
 
             int minimumElement = array[0];
             int minimumElementIndex = 0;
@@ -80,6 +94,8 @@ namespace FirstHomeworkSolution
             Console.WriteLine($"{minimumElement} - {minimumElementIndex}");
 
             Console.WriteLine(new string('-', 40));
+
+            // Task 6 - Find the multiplication and the substraction of the maximum and minumum elements of the array
 
             int min = int.MaxValue;
             int max = int.MinValue;
@@ -101,6 +117,8 @@ namespace FirstHomeworkSolution
             Console.WriteLine(max - min);
 
             Console.WriteLine(new string('-', 40));
+
+            // Task 7 - Find the count of the maximum element occurrences in the array
 
             int maximum = array[0];
             int maximumElementOccurrences = 0;
